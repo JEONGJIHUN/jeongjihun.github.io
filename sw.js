67bf23e0b96c0f3e3568ec3049b81e08242bb539
JEONGJIHUN <lippin21@gmail.com>
2020-06-22 22:15:46 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f4b74af5cea45dead1e6.js"
+    "url": "webpack-runtime-e26e41ce2cc827501274.js"
   },
   {
     "url": "styles.e6928981eee28274cf4d.css"
@@ -36,20 +36,20 @@ self.__precacheManifest = [
     "url": "styles-083a06cb5740baaf347d.js"
   },
   {
-    "url": "framework-88c48abae272a3bc68e7.js"
+    "url": "framework-99bce8781449cc5246e6.js"
   },
   {
-    "url": "532a2f07-24b41809a8ea87955a35.js"
+    "url": "532a2f07-1d2a7bbc1a68ccd5edd4.js"
   },
   {
-    "url": "app-95d068be855bde38e737.js"
+    "url": "app-e77413d79c8fad8a3468.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5c470c27c71ac53ce6dd3ff2d3850659"
+    "revision": "849ce47a3230beb1bc8ffd2702ad6f30"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b7e43e798a08777ea067.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fe3bb41d5112e8efbcdb.js"
   },
   {
     "url": "manifest.json",
@@ -150,7 +150,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-95d068be855bde38e737.js`))) {
+  if (!resources || !(await caches.match(`/app-e77413d79c8fad8a3468.js`))) {
     return await fetch(event.request)
   }
 
